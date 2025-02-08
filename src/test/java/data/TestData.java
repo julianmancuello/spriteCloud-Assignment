f0c0484bada56371fa6fba9b66e76d3baff85390
julianmancuello <162643807+julianmancuello@users.noreply.github.com>
2025-02-08 16:13:22 +0100

    testdata reorganized

--- src/test/java/data/TestData.java
+++ src/test/java/data/TestData.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class TestData {
 
+    //UI static data
     @Getter
     public enum UserType{
         STANDARD_USER("standard_user"),
@@ -20,21 +21,25 @@ public class TestData {
         }
     }
 
+    public static final String MASTER_PASSWORD = "secret_sauce";
+
     public static final String BASE_URL = "https://www.saucedemo.com/";
     public static final String CHECKOUT_URL = BASE_URL + "checkout-complete.html";
-
-    public static final String MASTER_PASSWORD = "secret_sauce";
 
     public static final String BLOCKED_USER_ERROR_MSG = "Epic sadface: Sorry, this user has been locked out.";
     public static final String NON_EXISTING_USER_ERROR_MSG = "Epic sadface: Username and password do not match any user in this service";
     public static final String SUCCESSFUL_HEADER = "Thank you for your order!";
 
+    //UI context data
     public static List<String> listOfAddedProducts;
 
-    public static UserList userList;
+    //API static data
     public static final UserData EMMA_WONG = new UserData(3, "emma.wong@reqres.in", "Emma", "Wong", "https://reqres.in/img/faces/3-image.jpg");
     public static final String ApiUser = "eve.holt@reqres.in";
     public static final String ApiPassword = "cityslicka";
+
+    //API context data
+    public static UserList userList;
     public static Token token;
     public static ErrorMessage errorMessage;
     public static InformationModified infoModified;
