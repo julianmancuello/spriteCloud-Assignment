02dc30a854f6f5d2461b993e96e9aa288a25b429
julianmancuello <162643807+julianmancuello@users.noreply.github.com>
2025-02-08 15:03:02 +0100

    added chromeoptions

--- src/main/java/ui/manager/DriverManager.java
+++ src/main/java/ui/manager/DriverManager.java
@@ -4,6 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Arrays;
 
 public class DriverManager {
 
@@ -12,11 +15,19 @@ public class DriverManager {
 
     public static void setUpWebDriver() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        driver = new ChromeDriver(setChromeOptions());
     }
 
     public static void tearDownWebDriver() {
         driver.quit();
+    }
+
+    private static ChromeOptions setChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        //options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
+        //options.addArguments("--start-maximized");
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        return options;
     }
 }
